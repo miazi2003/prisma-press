99bbf58a4173a90df9be8d2c1d6ff1131fe21471
Yeasin Miazi <yeasinmiazi1997@gmail.com>
2026-07-14 22:58:37 +0600

    feat : creation of access token by refresh token completed

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,40 +4,65 @@ import { prisma } from "../../lib/prisma";
 import { authService } from "./auth.service";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status";
+import { JwtPayload } from "jsonwebtoken";
 
 
 
 const loginUser = createAsync(async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
-    const {accessToken , refreshToken} = await authService.loginUser(payload)
+    const { accessToken, refreshToken } = await authService.loginUser(payload)
 
     //cookie set 
-    res.cookie("accessToken" , accessToken , {
-        httpOnly : true , 
-        secure : false , 
-        sameSite : "none" ,
-        maxAge : 1000 * 60 * 60 * 24
-    } )
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60 * 24
+    })
 
-    res.cookie("refreshToken" , refreshToken , {
-        httpOnly : true , 
-        secure : false , 
-        sameSite : "none" ,
-        maxAge : 1000 * 60 * 60 * 24 * 7
-    } )
-//response 
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    })
+    //response 
     sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
         message: "User logged in successfully",
-        data: { accessToken , refreshToken }
+        data: { accessToken, refreshToken }
 
     })
 
 })
 
 
+const refreshToken = createAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.refreshToken
+    console.log(token)
+    const {accessToken} = await authService.refreshToken(token)
+
+      res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60 * 24
+    })
+
+    sendResponse(res , {
+        success : true ,
+        statusCode : httpStatus.OK,
+        message : "Token refreshed successfully",
+        data : {
+            accessToken
+        }
+    })
+
+})
+
 
 export const authController = {
-    loginUser
+    loginUser,
+    refreshToken
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { SignOptions } from "jsonwebtoken";
+import { JwtPayload, SignOptions } from "jsonwebtoken";
 import config from "../../config";
 import { prisma } from "../../lib/prisma";
 import { jwtUtils } from "../../utils/jwt";
@@ -20,14 +20,14 @@ const loginUser = async (payload: ILoginUser) => {
         throw new Error("Password is incorrect")
     }
 
-const jwtPayload = {
-    id : user.id ,
-    name : user.name,
-    email : user.email,
-    role : user.role
-}
-    const accessToken = jwtUtils.createToken(jwtPayload , config.jwt_access_secret , config.jwt_access_expires_in as SignOptions)
-    const refreshToken = jwtUtils.createToken(jwtPayload , config.jwt_refersh_secret , config.jwt_refersh_expires_in as SignOptions)
+    const jwtPayload = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+    }
+    const accessToken = jwtUtils.createToken(jwtPayload, config.jwt_access_secret, config.jwt_access_expires_in as SignOptions)
+    const refreshToken = jwtUtils.createToken(jwtPayload, config.jwt_refersh_secret, config.jwt_refersh_expires_in as SignOptions)
 
 
 
@@ -37,6 +37,49 @@ const jwtPayload = {
     }
 }
 
+const refreshToken = async (refreshToken: string) => {
+
+    const verifiedRefreshToken = jwtUtils.verifyToken(refreshToken, config.jwt_refersh_secret)
+
+
+    if (!verifiedRefreshToken.success) {
+        throw new Error(verifiedRefreshToken.error)
+    }
+
+    const { id } = verifiedRefreshToken.data as JwtPayload;
+
+
+    const user = await prisma.user.findUniqueOrThrow({
+        where: {
+            id
+        }
+    })
+
+
+    if (user.activeStatus === "BLOOCKED") {
+        throw new Error("Your account has been blocked")
+    }
+
+    const jwtPayload = {
+        id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+    }
+
+
+    const accessToken = jwtUtils.createToken(jwtPayload, config.jwt_access_secret, config.jwt_refersh_expires_in as SignOptions);
+
+    return {
+        accessToken
+    };
+
+
+
+
+}
+
 export const authService = {
-    loginUser
+    loginUser,
+    refreshToken
 }
